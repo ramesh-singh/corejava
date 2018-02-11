f3a0078b7a61278842c1b52be950eaa15fbdeff9
Ramesh Singhq <rs376q@att.com>
2018-02-10 22:38:16 -0600

    Added initial version of Practice java project.

---
+++ Practice/src/com/mypackage/SampleCodes.java
@@ -0,0 +1,17 @@
+package com.mypackage;
+
+public class SampleCodes {
+	
+	//testing if variable name can contain special symbols after first letter
+	
+	//int test#;  ---Illegal identifier
+	//int test-;  ---Illegal identifier
+	int test$;
+	int test_;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
